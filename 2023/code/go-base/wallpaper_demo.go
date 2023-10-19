cbe3356ec139c0ccf6399066d302c84c88c199be
PFinal_南丞 <1370288056@qq.com>
2023-10-19 10:56:11 +0800

    2023-10-18 update

---
+++ 2023/code/go-base/wallpaper_demo.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/reujab/wallpaper"
+)
+
+/**
+ * @Author: PFinal南丞
+ * @Author: lampxiezi@163.com
+ * @Date: 2023/10/19
+ * @Desc:
+ * @Project: 2023
+ */
+
+func main() {
+	//background, err := wallpaper.Get()
+	//if err != nil {
+	//	fmt.Println(err)
+	//	panic(err)
+	//}
+	//fmt.Println(background)
+	//err = wallpaper.SetFromURL("https://cn.bing.com/th?id=OHR.RingEclipse_ZH-CN7063841581_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp")
+	//if err != nil {
+	//	fmt.Println(err)
+	//	panic(err)
+	//}
+	//err = wallpaper.SetMode(wallpaper.Crop)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	panic(err)
+	//}
+	cacheDir, err := wallpaper.GetCacheDir()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(cacheDir)
+}
