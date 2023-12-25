6dd9c25f5a153181db47e6dfbe7977aafe393cdf
PFinal_南丞 <1370288056@qq.com>
2023-12-25 17:43:19 +0800

    2023-12-25 更新

---
+++ 2023/code/go-base/channel_selector.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/**
+ * @Author: PFinal南丞
+ * @Author: lampxiezi@163.com
+ * @Date: 2023/12/25
+ * @Desc:
+ * @Project: 2023
+ */
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "two"
+	}()
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+}
