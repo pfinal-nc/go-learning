3630ed0d8457bc69786d7d1e09462b07f2cf9ea6
PFinal_南丞 <1370288056@qq.com>
2023-12-25 18:40:54 +0800

    2023-12-25 更新

---
+++ 2023/code/go-base/timeout_processing.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/**
+ * @Author: PFinal南丞
+ * @Author: lampxiezi@163.com
+ * @Date: 2023/12/25
+ * @Desc:
+ * @Project: 2023
+ */
+
+func main() {
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c1 <- "result 1"
+	}()
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(time.Second * 1):
+		fmt.Println("timeout 1")
+	}
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "result 2"
+	}()
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(time.Second * 3):
+		fmt.Println("timeout 2")
+	}
+}
