350b89eef65a6e82d0d390189a240d62c7c250ab
PFinal_南丞 <1370288056@qq.com>
2023-12-28 14:00:17 +0800

    2023-12-28 更新

--- 2023/code/go-base/composite_function.go
+++ 2023/code/go-base/composite_function.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 /**
  * @Author: PFinal南丞
  * @Author: lampxiezi@163.com
@@ -37,4 +42,35 @@ func All(vs []string, f func(string) bool) bool {
 		}
 	}
 	return true
+}
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
+
+func Map(vs []string, f func(string) string) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+func main() {
+	var strs = []string{"peach", "apple", "pear", "plum"}
+	fmt.Println(Index(strs, "pear"))
+	fmt.Println(Include(strs, "grape"))
+	fmt.Println(Any(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+	fmt.Println(All(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+	fmt.Println(Filter(strs, func(v string) bool {
+		return strings.Contains(v, "e")
+	}))
 }
