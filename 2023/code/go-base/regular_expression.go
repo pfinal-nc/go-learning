08f117c1f9bc1f10940c8c64c9fe289a36d93883
PFinal_南丞 <1370288056@qq.com>
2023-12-29 11:52:24 +0800

    2023-12-29 更新

---
+++ 2023/code/go-base/regular_expression.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+/**
+ * @Author: PFinal南丞
+ * @Author: lampxiezi@163.com
+ * @Date: 2023/12/29
+ * @Desc:
+ * @Project: 2023
+ */
+
+func main() {
+	match, _ := regexp.MatchString("p([a-z]+)ub", "pfinalclub")
+	fmt.Println(match)
+	r, _ := regexp.Compile("p([a-z]+)l")
+	fmt.Println(r.MatchString("pfinalclub"))
+	fmt.Println(r.FindString("pfinalclub"))
+	fmt.Println(r.FindStringIndex("pfinal"))
+	fmt.Println(r.FindStringSubmatch("pfinalclub")) // Submatch 返回完全匹配和局部匹配的字符串
+	fmt.Println(r.FindStringSubmatchIndex("pfinalclub"))
+	fmt.Println(r.FindAllString("pfinalclub", -1))
+}
