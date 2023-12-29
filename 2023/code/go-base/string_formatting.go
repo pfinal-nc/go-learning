2cec237f0f97f288e5826b8979034e5e914e473b
PFinal_南丞 <1370288056@qq.com>
2023-12-29 10:17:39 +0800

    2023-12-29 更新

--- 2023/code/go-base/string_formatting.go
+++ 2023/code/go-base/string_formatting.go
@@ -13,9 +13,16 @@ type point struct {
 	x, y int
 }
 
+// %#v 输出这个值的 go 语法表示
+// %T 打印值的类型
+// %d 使用十进制格式进行格式化
 func main() {
 	p := point{1, 2}
 	fmt.Printf("%v\n", p)
 	fmt.Printf("%+v\n", p)
 	fmt.Printf("%#v\n", p)
+	fmt.Printf("%T\n", p)
+	fmt.Printf("%t\n", true)
+	fmt.Printf("%d\n", 123)
+	fmt.Printf("%b\n", 14)
 }
