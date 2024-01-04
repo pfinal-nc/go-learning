93f2bcea267938ac9baccc2fb5669657e53a80d9
PFinal_南丞 <1370288056@qq.com>
2024-01-04 10:05:41 +0800

    2023-12-29 更新

--- 2023/code/go-base/randon_number.go
+++ 2023/code/go-base/randon_number.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 /**
@@ -18,4 +19,10 @@ func main() {
 	fmt.Println(rand.Intn(100))
 	fmt.Println(rand.Float64())
 	fmt.Print((rand.Float64()*5)+5, ",")
+	fmt.Println((rand.Float64() * 5) + 5)
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	fmt.Println(r1.Intn(100), ",")
+	fmt.Println(r1.Intn(100))
 }
